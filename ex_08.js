079e74dcb04f89b73a900621ae8b1d403e0e519d
sophia hardt <sophia_hardt@estudante.sesisenai.org.br>
2026-03-31 09:12:13 -0300

    add - fiz exercício 8

---
+++ ex_08.js
@@ -0,0 +1,14 @@
+function ex08() {
+
+    let valor1 = 4;
+    let valor2 = 7;
+
+    let diferenca = valor1 - valor2;
+    let calculo = (2 * valor1) + (3 * valor2);
+    let multiplicacao = valor1 * valor2;
+
+    console.log("Diferença:", diferenca);
+    console.log("Dobro da primeira + triplo da segunda:", calculo);
+    console.log("Multiplicação:", multiplicacao);
+
+}
