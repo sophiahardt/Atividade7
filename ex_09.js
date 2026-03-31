a43f699d78d48318e0856f7138969fc8d843574d
sophia hardt <sophia_hardt@estudante.sesisenai.org.br>
2026-03-31 09:17:38 -0300

    add - fiz exercício 9

---
+++ ex_09.js
@@ -0,0 +1,15 @@
+function ex09 () {
+
+    let num1 = 10;
+    let num2 = 15;
+
+    if (num1 > num2){
+        console.log(num1, num2);
+    }else{
+        console.log(num2, num1);
+    }
+
+}
+
+   
+
